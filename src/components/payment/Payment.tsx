13f0a37232bfed12d582cf994cacd74804e5b879
AbdunabiA <abdunabiabduvahobov86@gmail.com>
2024-03-02 16:30:21 +0500

    new

--- src/components/payment/Payment.tsx
+++ src/components/payment/Payment.tsx
@@ -8,7 +8,7 @@ const Payment = () => {
 	};
 	return (
 		<div>
-	      <a href={paymentUrl} style={{ color: "white" }}>Pay</a>
+	      <a href={paymentUrl} style={{ color: "white", marginTop:"100px", fontSize:"30px", textDecoration:"none", padding:"10px 24px", borderRadius:"10px", border:"1px solid white" }}>Нажмите для оплаты</a>
 	    </div>
 	)
 }
