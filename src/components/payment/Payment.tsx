d6e3d08888f3f59ea31cd39660f7065c55c13da3
AbdunabiA <abdunabiabduvahobov86@gmail.com>
2024-03-02 17:16:34 +0500

    new

--- src/components/payment/Payment.tsx
+++ src/components/payment/Payment.tsx
@@ -7,10 +7,22 @@ const Payment = () => {
 		paymentUrl: "test"
 	};
 	return (
-		<div>
-	      <a href={paymentUrl} style={{ color: "white", marginTop:"100px", fontSize:"30px", textDecoration:"none", padding:"10px 24px", borderRadius:"10px", border:"1px solid white" }}>Нажмите для оплаты</a>
-	    </div>
-	)
+    <div style={{ paddingTop: "100px" }}>
+      <a
+        href={paymentUrl}
+        style={{
+          color: "white",
+          fontSize: "30px",
+          textDecoration: "none",
+          padding: "10px 24px",
+          borderRadius: "10px",
+          border: "1px solid white",
+        }}
+      >
+        Нажмите для оплаты
+      </a>
+    </div>
+  );
 }
 
 export default Payment;
